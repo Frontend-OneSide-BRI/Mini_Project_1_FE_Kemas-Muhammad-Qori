69095be9b57c5edb9162dedf89f6e7769f85f44c
El <qorichsan@gmail.com>
2023-05-30 22:50:08 +0700

    add category data and assets

---
+++ assets/js/script.js
@@ -0,0 +1,114 @@
+const category = [
+    {
+        id: 1,
+        name: "Painting",
+        subCategory: [
+            {
+                painterName: "Leonardo Da Vinci",
+                creation: [
+                    {
+                        title: "Monalisa",
+                        year: "1452 - 1519",
+                        image: "",
+                    },
+                ],
+            },
+            {
+                painterName: "Raphael",
+                creation: [
+                    {
+                        title: "Madonna with the Fish",
+                        year: "1483 - 1520",
+                        image: "",
+                    },
+                ],
+            },
+            {
+                painterName: "Pablo Picasso",
+                creation: [
+                    {
+                        title: "Les Demoiselles d`Avignon",
+                        year: "1881 - 1973",
+                        image: "",
+                    },
+                ],
+            },
+            {
+                painterName: "Frida Kahlo",
+                creation: [
+                    {
+                        title: "Self-Portrait with Thorn Necklace dan Hummingbird",
+                        year: "1907 - 1954",
+                        image: "",
+                    },
+                ],
+            },
+            {
+                painterName: "Edvard Munch",
+                creation: [
+                    {
+                        title: "The Scream",
+                        year: "1863 - 1944",
+                        image: "",
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        id: 2,
+        name: "Statue",
+        subCategory: [{}],
+    },
+    {
+        id: 3,
+        name: "Film",
+        subCategory: [
+            {
+                directorName: "Quentin Tarantino",
+                creation: [
+                    {
+                        filmName: "",
+                        year: "",
+                    },
+                ],
+            },
+            {
+                directorName: "James Cameron",
+                creation: [
+                    {
+                        filmName: "",
+                        year: "",
+                    },
+                ],
+            },
+            {
+                directorName: "Christopher Nolan",
+                creation: [
+                    {
+                        filmName: "",
+                        year: "",
+                    },
+                ],
+            },
+            {
+                directorName: "Steven Spielberg",
+                creation: [
+                    {
+                        filmName: "",
+                        year: "",
+                    },
+                ],
+            },
+            {
+                directorName: "Martin Scorsese",
+                creation: [
+                    {
+                        filmName: "",
+                        year: "",
+                    },
+                ],
+            },
+        ],
+    },
+];
